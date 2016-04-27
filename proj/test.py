4c6a5cc91f3c4e6e096f07bee3ec96337feac365
Ken Zhang <kyz211@nyu.edu>
2016-04-26 20:16:47 -0400

    encapsulation, private vars, verb storage

--- proj/test.py
+++ proj/test.py
@@ -2,21 +2,24 @@ import nltk
 # presume select statement
 input = raw_input('What are you looking for?\n')
 
-tokens = nltk.word_tokenize(input)
-tagged_tokens = nltk.pos_tag(tokens)
-print(tagged_tokens)
+def parse(input):
+    tokens = nltk.word_tokenize(input)
+    tagged_tokens = nltk.pos_tag(tokens)
+    print tagged_tokens 
 
-retrieval = None
-target = None
-nouns = 0
-n_types = ['NN', 'NNS', 'NNP', 'NNPS']
-for i in range(len(tagged_tokens)):
-    token_list = list(tagged_tokens[i])
-    if token_list[1] in n_types: 
-        print('yes')
-    else:
-        print('no')
-        
+    retrieval = None
+    target = None
+    nouns = 0
+    n_types = ['NN', 'NNS', 'NNP', 'NNPS']
+    n_list = []
+    v_types = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+    v_list = []
+    for i in range(len(tagged_tokens)):
+        token_list = list(tagged_tokens[i])
+        if token_list[1] in n_types: 
+            n_list.append(token_list[0])
+    print n_list
+parse(input)
 # assuming the user writes proper english
 # else throw some error
 # one table attribute will always be there in the query
@@ -27,3 +30,6 @@ for i in range(len(tagged_tokens)):
 # if not NNP, it won't be in the form [VB] [NNP] [POS], therefore if there is POS, the format will be [function] [target] [retrieval]
 # otherwise, must be in the form [function] [retrieval] [target]
 # stripped of 's', target will always be a row
+# with 'computer science majors', last term will match the column after being stripped
+# then do a search of 'computer' and then 'computer science' in major column
+# with 'students majoring in computer science', 'NNS' means last noun phrase is a row
