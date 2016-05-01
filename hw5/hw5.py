7039a32045427bfa7d3c53cb70473da4e4a9e56f
Ken Zhang <kyz211@nyu.edu>
2016-04-30 21:21:02 -0400

    full baked term count and query clean

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -2,33 +2,41 @@ import nltk, sys, os, string
 from nltk import word_tokenize
 from collections import defaultdict
 
-closed_class_stop_words = ['a','the','an','and','or','but','about','above','after','along','amid','among',\
-                           'as','at','by','for','from','in','into','like','minus','near','of','off','on',\
-                           'onto','out','over','past','per','plus','since','till','to','under','until','up',\
-                           'via','vs','with','that','can','cannot','could','may','might','must',\
-                           'need','ought','shall','should','will','would','have','had','has','having','be',\
-                           'is','am','are','was','were','being','been','get','gets','got','gotten',\
-                           'getting','seem','seeming','seems','seemed',\
-                           'enough', 'both', 'all', 'your' 'those', 'this', 'these', \
-                           'their', 'the', 'that', 'some', 'our', 'no', 'neither', 'my',\
-                           'its', 'his' 'her', 'every', 'either', 'each', 'any', 'another',\
-                           'an', 'a', 'just', 'mere', 'such', 'merely' 'right', 'no', 'not',\
-                           'only', 'sheer', 'even', 'especially', 'namely', 'as', 'more',\
-                           'most', 'less' 'least', 'so', 'enough', 'too', 'pretty', 'quite',\
-                           'rather', 'somewhat', 'sufficiently' 'same', 'different', 'such',\
-                           'when', 'why', 'where', 'how', 'what', 'who', 'whom', 'which',\
-                           'whether', 'why', 'whose', 'if', 'anybody', 'anyone', 'anyplace', \
-                           'anything', 'anytime' 'anywhere', 'everybody', 'everyday',\
-                           'everyone', 'everyplace', 'everything' 'everywhere', 'whatever',\
-                           'whenever', 'whereever', 'whichever', 'whoever', 'whomever' 'he',\
-                           'him', 'his', 'her', 'she', 'it', 'they', 'them', 'its', 'their','theirs',\
-                           'you','your','yours','me','my','mine','I','we','us','much','and/or'
-                           ]
+#tfidf = term frequency (how many times it appears in query) x idf (total number of queries / number of queries where term occurs)
 
-new_tokens = []
-tfidf_hash = []
+def main():
+	new_tokens = []
+	word_hash = {}
+	tfidf_vector = []
+	closed_class_stop_words = ['a','the','an','and','or','but','about','above','after','along','amid','among',\
+	                           'as','at','by','for','from','in','into','like','minus','near','of','off','on',\
+	                           'onto','out','over','past','per','plus','since','till','to','under','until','up',\
+	                           'via','vs','with','that','can','cannot','could','may','might','must',\
+	                           'need','ought','shall','should','will','would','have','had','has','having','be',\
+	                           'is','am','are','was','were','being','been','get','gets','got','gotten',\
+	                           'getting','seem','seeming','seems','seemed',\
+	                           'enough', 'both', 'all', 'your' 'those', 'this', 'these', \
+	                           'their', 'the', 'that', 'some', 'our', 'no', 'neither', 'my',\
+	                           'its', 'his' 'her', 'every', 'either', 'each', 'any', 'another',\
+	                           'an', 'a', 'just', 'mere', 'such', 'merely' 'right', 'no', 'not',\
+	                           'only', 'sheer', 'even', 'especially', 'namely', 'as', 'more',\
+	                           'most', 'less' 'least', 'so', 'enough', 'too', 'pretty', 'quite',\
+	                           'rather', 'somewhat', 'sufficiently' 'same', 'different', 'such',\
+	                           'when', 'why', 'where', 'how', 'what', 'who', 'whom', 'which',\
+	                           'whether', 'why', 'whose', 'if', 'anybody', 'anyone', 'anyplace', \
+	                           'anything', 'anytime' 'anywhere', 'everybody', 'everyday',\
+	                           'everyone', 'everyplace', 'everything' 'everywhere', 'whatever',\
+	                           'whenever', 'whereever', 'whichever', 'whoever', 'whomever' 'he',\
+	                           'him', 'his', 'her', 'she', 'it', 'they', 'them', 'its', 'their','theirs',\
+	                           'you','your','yours','me','my','mine','I','we','us','much','and/or'
+	                           ]
+	file = open('cran/cran.qry', 'r')
+	raw = file.read()
+	sentences = clean(raw, closed_class_stop_words, new_tokens)
+	term_count(sentences, tfidf_vector, word_hash)
+	calc_tfidf(sentences, tfidf_vector, word_hash)                           
 
-def clean(text):
+def clean(text, closed_class_stop_words, new_tokens):
 	tokens = word_tokenize(text)
 	for token in tokens:
 		if token in closed_class_stop_words or token in string.punctuation or token == '.I' or token == '.W':
@@ -41,22 +49,19 @@ def clean(text):
 
 # [tf, total queries, queries with term, idf, tfidf]
 
-def calc_tfidf(text):
-	for sentence in text:
+def term_count(sentences, tfidf_vector, word_hash):
+	for sentence in sentences:
+		tfidf_vector.append([])
 		for word in sentence:
-			if tfidf_hash[word]:
-				# need list of query lists
-				tfidf_hash[word][0] += 1
-				tfidf_hash[word][2]  
+			word = word.lower()
+			if word_hash.get(word):
+				word_hash[word] += 1
 			else:
-				tfidf_hash[word] = [1, 225, 1, 225, 225]
+				word_hash[word] = 1
 
-file = open('cran/cran.qry', 'r')
-raw = file.read()
-sentences = clean(raw)
-print(sentences)
-#calc_tfidf(sentences)
+def calc_tfidf(sentences, tfidf_vector, word_hash):
+	print(sentences)
+	print(word_hash)
 
-#use hashmap
-#tfidf = term frequency (how many times it appears in query) x idf (total number of queries / number of queries where term occurs)
 
+main()
